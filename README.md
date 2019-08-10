2a14774a86d1aad16265f5268feecb802e442905
joshua <humanjboof@gmail.com>
2019-08-10 13:36:47 -0400

    added readme, links to view all from shoppage

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Coming Soon...
+Work in progress...
+
+* This is a test/demo app to showcase react, redux, stripe, sass, firebase
+* Will be refactor to styled components soon
+* Also context-api and hooks refactor coming soon
+* this app is deployed on firebase at the moment
+
+
+### [Visit the link here](https://parts-store-6b3fd.firebaseapp.com) 
--- src/components/Preview/Preview.component.jsx
+++ src/components/Preview/Preview.component.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 // @components
 import CollectionItem from '../Collection_item/Collection_item.component';
+// @svg
+import { ReactComponent as ViewMoreIcon } from '../../assets/img/logo/next.svg';
 // @styles
 import './Preview.styles.scss';
 
-const Preview = ({ title, items }) => (
+const viewMore = (route, history) => history.push(`/shop/${route}`);
+
+const Preview = ({ routeName, title, items, history }) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
@@ -14,8 +19,12 @@ const Preview = ({ title, items }) => (
                     <CollectionItem key={item.id} item={item} />
                 )
             }
+            <ViewMoreIcon
+                className='view-more'
+                onClick={() => viewMore(routeName, history)}
+            />
         </div>
     </div>
 );
 
-export default Preview;
+export default withRouter(Preview);
